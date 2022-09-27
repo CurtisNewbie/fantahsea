50f9eacdc7292f2c6c8db1dd95690e5ae3d9cdf8
yongj.zhuang <zhuangyongj@gmail.com>
2022-09-27 23:36:18 +0800

    Upgrade gocommon & organize scripts

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/curtisnewbie/fantahsea
 go 1.18
 
 require (
-	github.com/curtisnewbie/gocommon v0.0.0-20220924154817-8b079b333853
+	github.com/curtisnewbie/gocommon v0.0.0-20220926153007-89d0a351b4c1
 	github.com/gin-gonic/gin v1.8.1
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/sirupsen/logrus v1.9.0
@@ -13,6 +13,7 @@ require (
 require (
 	github.com/bsm/redislock v0.0.0-20191219095057-3d76f17a9f1e // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-co-op/gocron v1.17.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.0 // indirect
@@ -28,9 +29,11 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/nxadm/tail v1.4.8 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.0.0-20220826181053-bd7e27e6170d // indirect
 	golang.org/x/net v0.0.0-20220826154423-83b083e8dc8b // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20220825204002-c680a09ffe64 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
---
+++ scripts/schema.sql
@@ -0,0 +1,46 @@
+CREATE TABLE `gallery` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `gallery_no` varchar(32) NOT NULL DEFAULT '' COMMENT 'gallery no',
+  `user_no` varchar(64) NOT NULL DEFAULT '' COMMENT 'user''s no',
+  `name` varchar(255) NOT NULL COMMENT 'gallery name',
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'when the record is created',
+  `create_by` varchar(255) NOT NULL DEFAULT '' COMMENT 'who created this record',
+  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'when the record is updated',
+  `update_by` varchar(255) NOT NULL DEFAULT '' COMMENT 'who updated this record',
+  `is_del` tinyint NOT NULL DEFAULT '0' COMMENT '0-normal, 1-deleted',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `gallery_no_uniq` (`gallery_no`),
+  UNIQUE KEY `name_uk` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Gallery'
+
+
+ CREATE TABLE `gallery_image` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `gallery_no` varchar(32) NOT NULL DEFAULT '' COMMENT 'gallery no',
+  `image_no` varchar(32) NOT NULL DEFAULT '' COMMENT 'image no',
+  `name` varchar(255) NOT NULL COMMENT 'name of the file',
+  `file_key` varchar(255) NOT NULL COMMENT 'file key (file-service)',
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'when the record is created',
+  `create_by` varchar(255) NOT NULL DEFAULT '' COMMENT 'who created this record',
+  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'when the record is updated',
+  `update_by` varchar(255) NOT NULL DEFAULT '' COMMENT 'who updated this record',
+  `is_del` tinyint NOT NULL DEFAULT '0' COMMENT '0-normal, 1-deleted',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `image_no_uniq` (`image_no`),
+  UNIQUE KEY `gallery_no_file_key_uk` (`gallery_no`,`file_key`),
+  KEY `gallery_no_idx` (`gallery_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT="Gallery's Image"
+
+ CREATE TABLE `gallery_user_access` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `gallery_no` varchar(32) NOT NULL DEFAULT '' COMMENT 'gallery no',
+  `user_no` varchar(64) NOT NULL DEFAULT '' COMMENT 'user''s no',
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'when the record is created',
+  `create_by` varchar(255) NOT NULL DEFAULT '' COMMENT 'who created this record',
+  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'when the record is updated',
+  `update_by` varchar(255) NOT NULL DEFAULT '' COMMENT 'who updated this record',
+  `is_del` tinyint NOT NULL DEFAULT '0' COMMENT '0-normal, 1-deleted',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `gallery_user` (`gallery_no`,`user_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='User access to gallery'
+
