2d1c644c7345e34cfd4fb29034b87e97ff4a342d
yongj.zhuang <yongj.zhuang@outlook.com>
2022-10-20 13:50:42 +0800

    Upgrading gocommon

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.18
 
 require (
 	github.com/curtisnewbie/file-server-client-go v0.0.0-20221019161318-c723d3c89fb9
-	github.com/curtisnewbie/gocommon v0.0.0-20221019171507-7f39f27c2e5a
+	github.com/curtisnewbie/gocommon v0.0.0-20221020054819-e118883dadcd
 	github.com/gin-gonic/gin v1.8.1
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/sirupsen/logrus v1.9.0
--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"os"
+
 	"github.com/curtisnewbie/fantahsea/data"
 	"github.com/curtisnewbie/fantahsea/web/controller"
-	"github.com/curtisnewbie/gocommon/consul"
 	"github.com/curtisnewbie/gocommon/util"
 	"github.com/curtisnewbie/gocommon/web/server"
 	"github.com/gin-gonic/gin"
@@ -13,14 +14,13 @@ import (
 
 func main() {
 
-	_, conf := config.DefaultParseProfConf()
+	_, conf := config.DefaultParseProfConf(os.Args)
 
 	// register jobs
 	s := util.ScheduleCron("0 0/10 * * * *", data.CleanUpDeletedGallery)
 	s.StartAsync()
 
 	server.BootstrapServer(conf, func(router *gin.Engine) {
-		consul.RegisterDefaultHealthCheck(router)
 		controller.RegisterGalleryRoutes(router)
 		controller.RegisterGalleryImageRoutes(router)
 	})
