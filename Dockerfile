93ab3ada7e8c1ed295ad6d7a86ed8bf7c7690ff9
yongj.zhuang <zhuangyongj@gmail.com>
2023-05-27 15:09:13 +0800

    Modified:  Dockerfile Modified:  deploy.sh

--- Dockerfile
+++ Dockerfile
@@ -23,7 +23,7 @@ RUN go build -o main
 
 FROM alpine:3.17
 
-# for convert (change the cdn if necessary)
+# for convert (change the source if necessary)
 RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.tuna.tsinghua.edu.cn/g' /etc/apk/repositories
 RUN apk add file
 RUN apk add imagemagick
@@ -34,4 +34,4 @@ COPY --from=build /usr/share/zoneinfo /usr/share/zoneinfo
 
 ENV TZ=Asia/Shanghai
 
-CMD ["./main", "configFile=/usr/src/fantahsea/config/app-conf-prod.yml"]
+CMD ["./main", "app.name=fantahsea", "configFile=/usr/src/fantahsea/config/app-conf-prod.yml"]
--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
+GLOBIGNORE="fantahsea-tmp:.git:.vscode:fantahsea-base"
+
 # --------- remote
 remote="alphaboi@curtisnewbie.com"
 remote_path="~/services/fantahsea/build"
 # ---------
 
-#scp "run.sh" "${remote}:${remote_path}"
-#scp "Dockerfile" "${remote}:${remote_path}"
 
-# copy the config file just in case we updated it
-#scp "app-conf-dev.json" "${remote}:${remote_path}"
-
-GLOBIGNORE="fantahsea-tmp:.git:.vscode:fantahsea-base"
-
-# ssh  "alphaboi@curtisnewbie.com" "mv services/fantahsea/build services/fantahsea/build-bak"
+ssh  "alphaboi@curtisnewbie.com" "rm -rv ${remote_path}/*"
 
 scp app-conf-prod.yml "${remote}:~/services/fantahsea/config"
+if [ ! $? -eq 0 ]; then
+    exit -1
+fi
+
 scp -r * "${remote}:${remote_path}/"
 if [ ! $? -eq 0 ]; then
     exit -1
